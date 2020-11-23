21a5c5d43b8bd26c513b600860b0e980278c7e3e
nishizuishuaide <74659999+nishizuishuaide@users.noreply.github.com>
2020-11-23 12:02:44 +0800

    Create 112302.App.java

---
+++ 112302.App.java
@@ -0,0 +1,14 @@
+package pac;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) throws InterruptedException{			
+			while(true) {
+				Thread.sleep(1000);			
+				int x=(int) (Math.random()*100%100);
+				int y=(int) (Math.random()*100%100);
+				System.out.println("R的二维坐标"+"("+x+","+y+")");
+			}									
+	}
+}
