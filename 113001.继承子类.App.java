f4377122859aa15eca74900be7b47c699e0c318a
nishizuishuaide <74659999+nishizuishuaide@users.noreply.github.com>
2020-11-30 13:17:10 +0800

    继承子类

--- 113001.继承子类.App.java
+++ 113001.继承子类.App.java
@@ -3,11 +3,7 @@ package pac;
 public class App extends R{
 	public static void main(String[] args) {
 		huabei();
-		int array2d[][]= {
-				{1,2},
-				{3,4},
-				{5,6}
-		};
+
 		for (int row = 0; row < array2d.length; row++) {
 			for (int column = 0; column < array2d[0].length; column++) {
 				System.out.print(array2d[row][column]+" ");
