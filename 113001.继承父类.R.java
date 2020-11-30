d841cb66de74066861eafb916b8d419a9bc7b7e5
nishizuishuaide <74659999+nishizuishuaide@users.noreply.github.com>
2020-11-30 12:01:02 +0800

    Update 113001.继承父类.R.java

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -4,9 +4,19 @@ public class R {
 	static void huabei() {
 		String boy_filePath="img/boy.jpg";
 		String girl_filePath="img/girl.jpg";
-		int [][]array2d= {{1,2,3},{4,5,6}};
+		
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		System.out.println(array2d[1][2]);
+		int array2d[][]= {
+				{1,2},
+				{3,4},
+				{5,6}
+		};
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.print(array2d[row][column]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
