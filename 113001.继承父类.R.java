e95a01387e9eabfe930259ef8eafc0d7c2cc16e3
nishizuishuaide <74659999+nishizuishuaide@users.noreply.github.com>
2020-11-30 12:07:04 +0800

    继承父类

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -7,17 +7,6 @@ public class R {
 		
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		int array2d[][]= {
-				{1,2},
-				{3,4},
-				{5,6}
-		};
-		for (int row = 0; row < array2d.length; row++) {
-			for (int column = 0; column < array2d[0].length; column++) {
-				System.out.print(array2d[row][column]+" ");
-			}
-			System.out.println();
-		}
+
 	}
 }
-
