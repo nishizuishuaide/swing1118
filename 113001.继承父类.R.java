9eb84ff462bfeb1386559f779fa29aaed35fe69c
nishizuishuaide <74659999+nishizuishuaide@users.noreply.github.com>
2020-11-30 11:39:37 +0800

    继承父类

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,12 @@
+package pac;
+
+public class R {
+	static void huabei() {
+		String boy_filePath="img/boy.jpg";
+		String girl_filePath="img/girl.jpg";
+		int [][]array2d= {{1,2,3},{4,5,6}};
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(array2d[1][2]);
+	}
+}
