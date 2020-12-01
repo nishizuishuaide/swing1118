124cdf8f29905ddbb9b2a7bbed53ccb492424dca
nishizuishuaide <74659999+nishizuishuaide@users.noreply.github.com>
2020-12-01 23:35:33 +0800

    实现接口

--- 113002.实现接口.App.java
+++ 113002.实现接口.App.java
@@ -2,13 +2,8 @@ package pac;
 
 public class App implements R{
 	public static void main(String[] args) {
-		R.huabei();
-
-		int array2d[][]= {
-				{1,2},
-				{3,4},
-				{5,6}
-		};		
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);	
 		for (int row = 0; row < array2d.length; row++) {
 			for (int column = 0; column < array2d[0].length; column++) {
 				System.out.print(array2d[row][column]+" ");
